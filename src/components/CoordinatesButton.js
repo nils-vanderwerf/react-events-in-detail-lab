2143ca1de3229c1d7473d0dbd2f7927bbc39cd8d
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-04-06 09:17:38 +1000

    Created two buttons, one has an event, the other, a delayed event

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+
+export default class CoordinatesButton extends React.Component {
+    handleClick = event => {
+
+        console.log(this.props.onReceiveCoordinates([event.clientX, event.clientY]));
+    }
+    render() {
+        const button = document.querySelector('button');
+        
+        return (
+        <button onClick={this.handleClick}>Coords</button>
+        )
+        }
+    }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import ReactDOM from 'react-dom';
 import CoordinatesButton from './components/CoordinatesButton';
 import DelayedButton from './components/DelayedButton';
 
+
 ReactDOM.render(
   <div>
-    <CoordinatesButton onReceiveCoordinates={ mouseCoordinates => console.log(mouseCoordinates) } />
-    <DelayedButton onDelayedClick={ event => console.log(event) } delay={1500} />
+    <CoordinatesButton onReceiveCoordinates={ 
+      mouseCoordinates => console.log(mouseCoordinates) } />
+    <DelayedButton onDelayedClick={ event => console.log(`X coords are ${event.clientX}, X coords are ${event.clientY}`) } delay={1500} />
   </div>,
   document.getElementById('global')
 );
+
